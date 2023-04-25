ba8b06bc846f2eaedc4f04ded3d3319ac69ad4df
junrdev <juniorprogrammer09@gmail.com>
2023-04-25 21:42:59 +0300

    trying to clone the whatsapp ui101

---
+++ Whatsappui1/app/src/main/java/com/star/whatsappui1/HomeScreen.kt
@@ -0,0 +1,21 @@
+package com.star.whatsappui1
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.star.whatsappui1.ui.theme.HomeGreen
+
+@Composable
+fun HomeScreen() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(HomeGreen),){
+
+
+
+    }
+}
---
+++ Whatsappui1/app/src/main/java/com/star/whatsappui1/TopNaVBar.kt
@@ -0,0 +1,50 @@
+package com.star.whatsappui1
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TopNav() {
+
+    Box(modifier = Modifier.fillMaxWidth()){
+
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+//                .padding(15.dp)
+        ) {
+            Text(
+                text = "WhatsApp",
+                fontSize = 10.sp,
+                modifier = Modifier
+                    .padding(7.5.dp)
+            )
+
+            Icon(
+                painter = painterResource(id = R.drawable.camera_24),
+                contentDescription = "camera icon",
+                modifier = 
+
+            )
+        }
+
+        Row() {
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TopNavPreview() {
+    TopNav()
+}
