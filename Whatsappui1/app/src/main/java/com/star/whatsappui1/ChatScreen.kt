d769a47e0b32c9863b8d3d898ed4487cd65f8248
junrdev <juniorprogrammer09@gmail.com>
2023-04-26 22:06:58 +0300

    edits

--- Whatsappui1/app/src/main/java/com/star/whatsappui1/ChatScreen.kt
+++ Whatsappui1/app/src/main/java/com/star/whatsappui1/ChatScreen.kt
@@ -1,8 +1,10 @@
 package com.star.whatsappui1
 
+import android.content.Context
 import android.graphics.Paint.Align
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
@@ -10,7 +12,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -23,20 +25,34 @@ import com.star.whatsappui1.ui.theme.ChatBack
 import com.star.whatsappui1.ui.theme.TextWhite
 
 @Composable
-fun ChatsScreen(chats : List<Chat>) {
+fun ChatsScreen(chats : List<Chat>, context: Context) {
 
     LazyColumn(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier){
         items(chats.size){
-            ChatItem(chat = chats[it])
-            Spacer(modifier = Modifier.size(2.dp).padding(start = 10.dp))
+            ChatItem(
+                chat = chats[it],
+                modifier = Modifier.clickable {
+
+                }
+            )
+
+
+            Spacer(
+                modifier = Modifier
+                    .size(2.dp)
+                    .padding(start = 10.dp)
+            )
         }
     }
 }
 
 @Composable
-fun ChatItem(chat : Chat) {
+fun ChatItem(chat : Chat,modifier: Modifier=Modifier) {
 
-    Box(modifier = Modifier
+    var username by remember {
+        mutableStateOf(chat.content)
+    }
+    Box(modifier
         .fillMaxWidth()
         .background(ChatBack)
         .padding(5.dp),
@@ -50,6 +66,7 @@ fun ChatItem(chat : Chat) {
                     .clip(RoundedCornerShape(20.dp))
                     .align(Alignment.CenterStart)
                     .border(width = 2.dp, MaterialTheme.colors.primary, shape = CircleShape)
+
             ){
 
                 Icon(
