8df1319b66785a60cd07ed887b6265d3d6c1ee6c
junrdev <juniorprogrammer09@gmail.com>
2023-04-26 22:59:40 +0300

    fixed a minor issue

--- TwitterUI1/app/src/main/java/com/star/twitterui1/MainActivity.kt
+++ TwitterUI1/app/src/main/java/com/star/twitterui1/MainActivity.kt
@@ -7,17 +7,12 @@ import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.star.twitterui1.ui.theme.*
-import java.time.LocalDate
-import java.time.LocalTime
 
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
@@ -58,16 +53,86 @@ fun TopNavBar() {
  * Tweet(
  */
 
-fun demo() : List<Tweet>{
-    var tweets = listOf(
-        Tweet(dateTweeted ="", username = "Brian Codes", tweetContent = "Hello world", timeTweeted = "", likes = 20, retweets = 10, comments = 30, username_at = "@dev_brianke", analytics = 100),
-        Tweet(username = "User 1", username_at = "@user_1", tweetContent = "Bro! WTF", likes = 25, comments = 23, retweets = 50, dateTweeted = "", timeTweeted = "", analytics = 23),
-        Tweet(username = "Mulla", username_at = "@moonlight", tweetContent = "My G", likes = 80, comments = 20, retweets = 45, dateTweeted = "", timeTweeted = "", analytics = 60),
-        Tweet(username = "La Goat", username_at = "@goat", tweetContent = "This UI Crazy", likes = 250, comments = 90, retweets = 44, dateTweeted = "", timeTweeted = "", analytics = 30),
-        Tweet(username = "Tano Nane", username_at = "@matata", tweetContent = "Good Job Bro", likes = 50, comments = 90, retweets = 64, dateTweeted = "", timeTweeted = "", analytics = 40),
-        Tweet(username = "Messy Chris", username_at = "@messy", tweetContent = "SiuuUUU ?!?!", likes = 900, comments = 290, retweets = 900, dateTweeted = "", timeTweeted = "", analytics = 500)
+fun demo(): List<Tweet> {
+
+    return listOf(
+        Tweet(
+            dateTweeted = "",
+            username = "Brian Codes",
+            tweetContent = "Hello world",
+            timeTweeted = "",
+            likes = 20,
+            retweets = 10,
+            comments = 30,
+            username_at = "@dev_brianke",
+            analytics = 100
+        ),
+        Tweet(
+            username = "User 1",
+            username_at = "@user_1",
+            tweetContent = "Bro! WTF",
+            likes = 25,
+            comments = 23,
+            retweets = 50,
+            dateTweeted = "",
+            timeTweeted = "",
+            analytics = 23
+        ),
+        Tweet(
+            username = "Mulla",
+            username_at = "@moonlight",
+            tweetContent = "My G\nMy Bro\nWhatsApp",
+            likes = 80,
+            comments = 20,
+            retweets = 45,
+            dateTweeted = "",
+            timeTweeted = "",
+            analytics = 60
+        ),
+        Tweet(
+            username = "La Goat",
+            username_at = "@goat",
+            tweetContent = "This UI Crazy",
+            likes = 250,
+            comments = 90,
+            retweets = 44,
+            dateTweeted = "",
+            timeTweeted = "",
+            analytics = 30
+        ),
+        Tweet(
+            username = "Tano Nane",
+            username_at = "@matata",
+            tweetContent = "Good Job Bro",
+            likes = 50,
+            comments = 90,
+            retweets = 64,
+            dateTweeted = "",
+            timeTweeted = "",
+            analytics = 40
+        ),
+        Tweet(
+            username = "Messy Chris",
+            username_at = "@messy",
+            tweetContent = "SiuuUUU ?!?!",
+            likes = 900,
+            comments = 290,
+            retweets = 900,
+            dateTweeted = "",
+            timeTweeted = "",
+            analytics = 500
+        ),
+        Tweet(
+            username = "Virgil",
+            username_at = "@albo",
+            tweetContent = "Albo described the color that separates bl!ck and wh!te as Offwhite",
+            likes = 900,
+            comments = 290,
+            retweets = 900,
+            dateTweeted = "",
+            timeTweeted = "",
+            analytics = 500
+        )
 
     )
-    
-    return tweets
 }
--- TwitterUI1/app/src/main/java/com/star/twitterui1/TweetUI.kt
+++ TwitterUI1/app/src/main/java/com/star/twitterui1/TweetUI.kt
@@ -3,6 +3,7 @@ package com.star.twitterui1
 import android.graphics.Paint.Align
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
@@ -10,7 +11,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -36,6 +37,9 @@ fun LoadTweets(list: List<Tweet>) {
 fun TweetUI(tweet: Tweet) {
 
 
+    var contentClicked by remember {
+        mutableStateOf(false)
+    }
     Box(modifier = Modifier
         .fillMaxWidth()
         .background(
@@ -98,76 +102,92 @@ fun TweetUI(tweet: Tweet) {
             }
         }
 
-        //content
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 50.dp)
-                .border(
-                    shape = RoundedCornerShape(5.dp),
-                    color = TweetCardContentColor,
-                    width = 2.dp
-                ),
-        ) {
+        Column {
 
-            Row (modifier = Modifier.padding(10.dp)){
-                Text(text =tweet.tweetContent, color = TextWhite)
+            //content
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 50.dp)
+                    .border(
+                        shape = RoundedCornerShape(5.dp),
+                        color = TweetCardContentColor,
+                        width = 2.dp
+                    ).clickable {
+                    contentClicked = !contentClicked
+                },
+            ) {
+
+                Row (modifier = Modifier.padding(10.dp)){
+                    Text(
+                        color = TextWhite,
+                        maxLines = if(contentClicked) 4 else 1,
+                        text = tweet.tweetContent,
+                    )
+                }
+
             }
 
+            //likes, comments, retweets and analytics
+            Row(
+                modifier = Modifier.padding(top = 10.dp, start = 20.dp),
+                horizontalArrangement = Arrangement.Center,
+            ) {
+
+                //likes
+                Row(modifier = Modifier.padding(end = 20.dp)){
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_heart_24),
+                        contentDescription = "unlikes",
+                        tint = TweetLikeColor,
+                        modifier = Modifier
+                            .padding(end = 5.dp)
+                            .size(20.dp)
+
+                    )
+                    Text(text = "${tweet.likes}k", fontSize = 12.sp, color = TextWhite, modifier = Modifier)
+                }
+
+                //retweets
+                Row(modifier = Modifier.padding(end = 20.dp)) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_repeat_24),
+                        contentDescription = "unretweets",
+                        tint = MaterialTheme.colors.primary,
+                        modifier = Modifier
+                            .padding(end = 5.dp)
+                            .size(20.dp)
+                    )
+                    Text(text = "${tweet.retweets}m", fontSize = 12.sp, color = TextWhite)
+                }
+
+                //comments
+                Row (modifier = Modifier.padding(end = 17.dp)){
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_message_24),
+                        contentDescription = "uncomments",
+                        tint = MaterialTheme.colors.primary,
+                        modifier = Modifier
+                            .padding(end = 5.dp)
+                            .size(20.dp)
+                    )
+                    Text(text = "${tweet.comments}m", fontSize = 12.sp, color = TextWhite)
+                }
+
+                //analytics
+                Row (modifier = Modifier){
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_signal_cellular_alt_24),
+                        contentDescription = "unalytics",
+                        tint = MaterialTheme.colors.primary,
+                        modifier = Modifier
+                            .padding(end = 3.dp)
+                            .size(20.dp)
+                    )
+                    Text(text = "${tweet.analytics}m", fontSize = 12.sp, color = TextWhite)
+                }
+            }
         }
 
-        //likes, comments, retweets and analytics
-        Row(
-            modifier = Modifier.padding(top = 110.dp, start = 20.dp),
-            horizontalArrangement = Arrangement.Center,
-        ) {
-
-            //likes
-            Row(modifier = Modifier.padding(end = 20.dp)){
-                Icon(
-                    painter = painterResource(id = R.drawable.baseline_heart_24),
-                    contentDescription = "unlikes",
-                    tint = TweetLikeColor,
-                    modifier = Modifier.padding(end = 5.dp).size(20.dp)
-
-                )
-                Text(text = "${tweet.likes}k", fontSize = 12.sp, color = TextWhite, modifier = Modifier)
-            }
-
-            //retweets
-            Row(modifier = Modifier.padding(end = 20.dp)) {
-                Icon(
-                    painter = painterResource(id = R.drawable.baseline_repeat_24),
-                    contentDescription = "unretweets",
-                    tint = MaterialTheme.colors.primary,
-                    modifier = Modifier
-                        .padding(end = 5.dp).size(20.dp)
-                )
-                Text(text = "${tweet.retweets}m", fontSize = 12.sp, color = TextWhite)
-            }
-
-            //comments
-            Row (modifier = Modifier.padding(end = 17.dp)){
-                Icon(
-                    painter = painterResource(id = R.drawable.baseline_message_24),
-                    contentDescription = "uncomments",
-                    tint = MaterialTheme.colors.primary,
-                    modifier = Modifier
-                        .padding(end = 5.dp).size(20.dp)
-                )
-                Text(text = "${tweet.comments}m", fontSize = 12.sp, color = TextWhite)
-            }
-
-            //analytics
-            Row (modifier = Modifier){
-                Icon(
-                    painter = painterResource(id = R.drawable.baseline_signal_cellular_alt_24),
-                    contentDescription = "unalytics",
-                    tint = MaterialTheme.colors.primary,
-                    modifier = Modifier.padding(end = 3.dp).size(20.dp)
-                )
-                Text(text = "${tweet.analytics}m", fontSize = 12.sp, color = TextWhite)
-            }
-        }
     }
 }
