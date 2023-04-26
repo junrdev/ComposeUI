e8d69c90c2097825a577e764be489ad0f5cf8b5c
junrdev <juniorprogrammer09@gmail.com>
2023-04-26 18:28:21 +0300

    changes the UI flow

--- Whatsappui1/app/src/main/java/com/star/whatsappui1/HomeScreen.kt
+++ Whatsappui1/app/src/main/java/com/star/whatsappui1/HomeScreen.kt
@@ -1,12 +1,20 @@
 package com.star.whatsappui1
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.star.whatsappui1.ui.theme.FabBg
 import com.star.whatsappui1.ui.theme.HomeGreen
 
 @Composable
@@ -15,6 +23,49 @@ fun HomeScreen() {
         .fillMaxSize()
         .background(HomeGreen),){
 
+        Column {
+
+            Row {
+                TopNav()
+            }
+
+            Column {
+                ChatsScreen(
+                    listOf(
+                        Chat(content = "Hello world", sender = "Brian", timeSent = "07:30"),
+                        Chat(content = "Hello world", sender = "Brian", timeSent = "07:30"),
+                        Chat(content = "Hello world", sender = "Brian", timeSent = "07:30"),
+                        Chat(content = "Hello world", sender = "Brian", timeSent = "07:30"),
+                        Chat(content = "Hello world", sender = "Brian", timeSent = "07:30"),
+                        Chat(content = "Hello world", sender = "Brian", timeSent = "07:30"),
+                        Chat(content = "Hello world", sender = "Brian", timeSent = "07:30"),
+                        Chat(content = "Hello world", sender = "Brian", timeSent = "07:30"),
+                        Chat(content = "Hello world", sender = "Brian", timeSent = "07:30"),
+                        Chat(content = "Hello world", sender = "Brian", timeSent = "07:30"),
+                        Chat(content = "Hello world", sender = "Brian", timeSent = "07:30"),
+                        Chat(content = "Hello world", sender = "Brian", timeSent = "07:30"),
+                        Chat(content = "Hello world", sender = "Brian", timeSent = "07:30")
+                    )
+                )
+
+            }
+        }
+
+//        Fab
+        Row (
+            modifier = Modifier.align(Alignment.BottomEnd).fillMaxSize().padding(end = 40.dp, bottom = 40.dp),
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.Bottom
+        ){
+            Icon(
+                painter = painterResource(id = R.drawable.add_circle_24),
+                contentDescription = null,
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(60.dp),
+                tint = FabBg
+            )
+        }
 
 
     }
